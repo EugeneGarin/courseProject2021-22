1d223d25747583b4cd12ae59e4bde296123b40ad
Eugene <diterecs@gmail.com>
2021-12-08 20:06:47 +0200

    added fullstack we dev test and test results page

--- public/js/app.min.js
+++ public/js/app.min.js
@@ -9,13 +9,44 @@
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var alpinejs__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! alpinejs */ "./node_modules/alpinejs/dist/module.esm.js");
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 /**
 * Import and initialize AlpineJS
 */
 
 window.Alpine = alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"];
-alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"].start(); // document.addEventListener('DOMContentLoaded', () => {
-// });
+alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"].start();
+document.addEventListener('DOMContentLoaded', function () {
+  var form = document.querySelector("form");
+  var log = document.querySelector("#log");
+  form.addEventListener("submit", function (event) {
+    var data = new FormData(form);
+    var output = "";
+
+    var _iterator = _createForOfIteratorHelper(data),
+        _step;
+
+    try {
+      for (_iterator.s(); !(_step = _iterator.n()).done;) {
+        var entry = _step.value;
+        output = output + entry[0] + "=" + entry[1] + "\r";
+      }
+    } catch (err) {
+      _iterator.e(err);
+    } finally {
+      _iterator.f();
+    }
+
+    ;
+    log.innerText = output;
+    event.preventDefault();
+  }, false);
+});
 
 /***/ }),
 
--- src/js/app.js
+++ src/js/app.js
@@ -5,8 +5,19 @@ import Alpine from 'alpinejs'
 window.Alpine = Alpine;
 Alpine.start();
 
-// document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
+    let form = document.querySelector("form");
+    let log = document.querySelector("#log");
 
+    form.addEventListener("submit", function(event) {
+    let data = new FormData(form);
+    let output = "";
+    for (const entry of data) {
+        output = output + entry[0] + "=" + entry[1] + "\r";
+    };
+    log.innerText = output;
+    event.preventDefault();
+    }, false);
 
-// });
+});
